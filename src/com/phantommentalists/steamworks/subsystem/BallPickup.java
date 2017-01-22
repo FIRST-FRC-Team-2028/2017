0f40e2a8da994ed0ca77211de60d2aab17042d24
guyknur <guyknur@gmail.com>
2017-01-22 17:13:20 -0500

    Delete BallPickup.java
    No longer Needed

--- src/com/phantommentalists/steamworks/subsystem/BallPickup.java
+++
@@ -1,8 +0,0 @@
-package com.phantommentalists.steamworks.subsystem;
-
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-import edu.wpi.first.wpilibj.command.Subsystem;
-
-@objid ("10bd9107-945b-4bf8-984b-3efcf8c8b8fd")
-public class BallPickup extends Subsystem {
-}
