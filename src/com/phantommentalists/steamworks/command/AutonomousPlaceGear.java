902fef6cd7d53284cf9541c956e266401073dcab
588954 <588954@BAH5CD6310GHC.resource.ds.bah.com>
2017-01-26 16:19:53 -0500

    Fixed missing commands

---
+++ src/com/phantommentalists/steamworks/command/AutonomousPlaceGear.java
@@ -0,0 +1,26 @@
+package com.phantommentalists.steamworks.command;
+
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+import edu.wpi.first.wpilibj.command.Command;
+
+@objid ("12a1f2a1-7499-4d22-a067-e848fc68ddd1")
+public class AutonomousPlaceGear extends Command {
+    @objid ("1994ab88-6379-4344-be87-4327b01a4d54")
+    protected void execute() {
+    }
+
+    @objid ("64cf8f26-d960-4b74-956a-003261b02ec4")
+    protected boolean isFinished() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @objid ("d87d12ff-7b1d-4212-974a-569d50b7826b")
+    protected void initialize() {
+    }
+
+    @objid ("d18e2ac9-77f7-4551-9d7d-f15982c50ee4")
+    public AutonomousPlaceGear() {
+    }
+
+}
---
+++ src/com/phantommentalists/steamworks/command/CrabDriveCommand.java
@@ -0,0 +1,33 @@
+package com.phantommentalists.steamworks.command;
+
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+@objid ("9fa60e62-3aa8-4cba-95d7-0fa18c5388a6")
+public class CrabDriveCommand extends Command {
+
+	private Subsystem drivetrain;
+	
+	@objid ("1e72b4ad-7dfd-4b83-84be-fa89d3252097")
+    public CrabDriveCommand(Subsystem drivetrain) {
+    }
+
+    @objid ("541d2e03-3fdd-4cfa-9d1e-d2aa9f5c7a3a")
+    @Override
+    protected void execute() {
+    }
+
+    @objid ("f3162078-8211-432f-9b18-0690658db05f")
+    @Override
+    protected boolean isFinished() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @objid ("f6732873-ee2c-406b-a251-0da95d1d2e0d")
+    @Override
+    protected void initialize() {
+    }
+
+}
